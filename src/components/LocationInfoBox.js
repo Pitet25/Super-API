f78e84db65c1166576f37999cdf2455e650b3929
Pitet25 <Pitet25@wp.pl>
2021-05-16 21:01:17 +0200

    Add files via upload

---
+++ src/components/LocationInfoBox.js
@@ -0,0 +1,13 @@
+const LocationInfoBox = ({ info }) => {
+    return (
+        <div className="location-info">
+            <h2>Informacje o lokacji</h2>
+            <ul>
+                <li>ID: <strong>{info.id}</strong></li>
+                <li>Tytul: <strong>{info.title}</strong></li>
+            </ul>
+        </div>
+    )
+}
+
+export default LocationInfoBox
